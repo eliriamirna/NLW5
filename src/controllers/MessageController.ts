ac1b4b685bed6f648734b6b5b6f0c1caf7f92f3d
Eliria Mirna <eliriadias@gmail.com>
2021-05-04 08:26:49 -0300

    atualizado

---
+++ src/controllers/MessageController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { MessageService } from "../services/MessageService"
+
+class MessageController {
+    async create(request: Request, response: Response) {
+        const { admin_id, text, user_id } = request.body;
+        const messageService = new MessageService();
+
+        const message = await messageService.create({
+            admin_id,
+            text,
+            user_id,
+        });
+
+        return response.json(message);
+    }
+
+    // localhost:3333/messages/idDoUsuario
+    async showByUser(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const messageService = new MessageService();
+
+        const list = await messageService.listByUser(id);
+
+        return response.json(list);
+    }
+}
+
+export { MessageController };
